b835d7b37042ee99113f2ce18cd2c3b1472682b3
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2020-01-04 17:12:04 +0100

    Work with dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+nerd_or_cool = {
+    "Star Wars" : "Cool",
+    "Star Trek" : "Nerd",
+    "Manga" : "Weeb",
+    "You" : "Are amazing" }
+
+print(nerd_or_cool)
+print(nerd_or_cool["Star Wars"])
+print(nerd_or_cool["You"])
